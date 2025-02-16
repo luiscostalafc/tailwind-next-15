6735dc03e5b68cef92ec2b24f944a9aade44f2c3
Luis Costa <luiscostalafc@gmail.com>
2025-02-16 07:14:28 -0300

    feat: include eslint-tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends(
     "next/core-web-vitals",
     "next/typescript",
-    "@rocketseat/eslint-config/next"
+    "plugin:tailwindcss/recommended"
   ),
 ];
 
